524025e8549140230f099c10c8d85f09e10a398a
Albert <albert.1701920351@ucaldas.edu.co>
2025-05-27 16:30:12 -0500

    Main deploy fix

--- main.py
+++ main.py
@@ -184,3 +184,11 @@ def pico_y_placa():
         6: []
     }
     return {"dia": dia, "placas_restringidas": pico[dia]}
+
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
