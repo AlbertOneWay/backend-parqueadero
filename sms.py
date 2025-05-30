428906cef45f372f24c8d54686c6fbbe340de95d
Albert <albert.1701920351@ucaldas.edu.co>
2025-05-30 14:23:39 -0500

    MSG Twilio

--- sms.py
+++ sms.py
@@ -11,11 +11,9 @@ twilio_from = os.getenv("TWILIO_FROM")
 client = Client(twilio_sid, twilio_token)
 
 def enviar_sms(destino: str, mensaje: str):
-    # Comentado para pruebas: retorna un mensaje simulado
-    # message = client.messages.create(
-    #     body=mensaje,
-    #     from_=twilio_from,
-    #     to=destino
-    # )
-    # return message.sid
-    return f"SMS simulado a {destino}: {mensaje}"
+    message = client.messages.create(
+        body=mensaje,
+        from_=twilio_from,
+        to=destino
+    )
+    return message.sid
